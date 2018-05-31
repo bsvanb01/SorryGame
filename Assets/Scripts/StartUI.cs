892258bda786abce016282d3b067fc8765cb04eb
Brad Van Buren <bvanburen14@outlook.com>
2018-05-31 09:37:17 -0400

    First Commit

---
+++ Assets/Scripts/StartUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneManager.LoadScene("Sorry_Board_1");
+    }
+	
+
+}
