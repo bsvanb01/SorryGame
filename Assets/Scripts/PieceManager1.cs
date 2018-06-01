d3551059650ae78a73769c0793d6b8eea805bcc6
Brad Van Buren <bvanburen14@outlook.com>
2018-06-01 14:24:44 -0400

    BIG update

---
+++ Assets/Scripts/PieceManager1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceManager1 : MonoBehaviour {
+
+
+    [SerializeField] private GameObject p1Piece1;
+    [SerializeField] private GameObject p1Piece2;
+    [SerializeField] private GameObject p1Piece3;
+    [SerializeField] private GameObject p1Piece4;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PieceManager2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceManager2 : MonoBehaviour {
+
+    [SerializeField] private GameObject p2Piece1;
+    [SerializeField] private GameObject p2Piece2;
+    [SerializeField] private GameObject p2Piece3;
+    [SerializeField] private GameObject p2Piece4;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
