053cc0126ed9ee60f2b69adfdce9ac6c86e910cd
ONBASE\jzucca <jzucca>
2018-06-04 13:56:55 -0400

    added 4 buttons to screen, started movement script, added cardCount in cardDeck

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void move(int cardNum)
+    {
+
+    }
+
+
+    public bool checkMovement(int card, int player)
+    {
+        bool anyMovement = false;
+        Renderer piece = GetComponent<Renderer>();
+
+        #region move from start
+        if(card == 1 || card == 2)
+        {
+            GameObject startButton = GameObject.Find("Move From Start");
+            //make button visible
+
+            //if(!piece.squareType.Equals("start"))
+            //  startButton.GetComponent<Button>().interactable = false;
+            //else
+            //  startButton.GetComponent<Button>().interactable = true;
+            //  anyMovement = true;
+        }
+        #endregion
+
+        #region move forward
+        if (card == 1 || card == 2 || card == 3 || card == 5 || card == 7 || card == 8 || card == 10 || card == 11 || card == 12)
+        {
+            GameObject forButton = GameObject.Find("Move Forward");
+            //make button visible
+
+            //if(piece.squareType == normal && piece.curSquare > (player.startSquare + 50) % 60 && piece.curSquare < (player.startSquare + 60) % 60)
+            //{
+            //    if(piece.curSquare + card > player.startSquare + 65)
+            //    {
+            //        forButton.GetComponent<Button>().interactable = false;
+            //    }
+            //    else
+            //    {
+            //        forButton.GetComponent<Button>().interactable = true;
+            //        anyMovement = true;
+            //    }
+            //}
+            //else if (piece.squareType == "safe")
+            //{
+            //    if(piece.curSquare + card > 5)
+            //        forButton.GetComponent<Button>().interactable = false;
+            //    else
+            //    {
+            //        forButton.GetComponent<Button>().interactable = true;
+            //        anyMovement = true;
+            //    }
+            //}
+            //else if (piece.squareType == "home")
+            //{
+            //    forButton.GetComponent<Button>().interactable = false;
+            //}
+            //else
+            //{
+            //    forButton.GetComponent<Button>().interactable = true;
+            //    anyMovement = true;
+            //}
+        }
+        #endregion
+
+        #region move backward
+        if(card == 4 || card == 10)
+        {
+            GameObject backButton = GameObject.Find("Move Backward");
+            //make button visible
+            backButton.GetComponent<Button>().interactable = true;
+            anyMovement = true;
+        }
+        #endregion
+
+        #region swap sorry
+        if(card == 13)
+        {
+            //if(piece.squareType != "start")
+            //else if (all other players pieces are not in normal spaces
+            //else
+                    //anyMovement = true;
+        }
+        #endregion
+
+        #region swap 11
+        if (card == 11)
+        {
+            GameObject swapButton = GameObject.Find("Swap");
+            //make button visible
+
+            //if(piece.squareType!=normal)
+            //swapButton.GetComponent<Button>().interactable = false;
+            //else if(FindObjectsOfTypeAll other players pieces aren't normal)
+            //swapButton.GetComponent<Button>().interactable = false;
+            //else
+            //anyMovement = true;
+            //swapButton.GetComponent<Button>().interactable = true;
+        }
+        #endregion
+        return anyMovement;
+    }
+}
