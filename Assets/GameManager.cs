c259c920ebee5fd2bc35b20be1e9a2c88d9996d3
ONBASE\jzucca <jzucca>
2018-06-07 10:06:21 -0400

    Added player turns and updated Game Manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,26 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     // Use this for initialization
 
-    [SerializeField] private GameObject player1;
-    [SerializeField] private GameObject player2;
-    [SerializeField] private GameObject player3;
-    [SerializeField] private GameObject player4;
+    [SerializeField]
+    private GameObject player1;
+    [SerializeField]
+    private GameObject player2;
+    [SerializeField]
+    private GameObject player3;
+    [SerializeField]
+    private GameObject player4;
     public static GameObject currentPiece;
-    public static int currentPlayer;
+    public static int currentPlayer = 0;
     public static int currentSquare;
 
     public GameObject[] spaces = new GameObject[84];
 
-    void Start () {
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayerChange()
+    {
+        currentPlayer = currentPlayer % 4;
+
+        if (currentPlayer == 0)             //Player 1 turn
+        {
+            Debug.Log("Player 1 turn");
+            PieceManager1.player1Active = true;
+            PieceManager2.player2Active = false;
+            PieceManager3.player3Active = false;
+            PieceManager4.player4Active = false;
+        }
+
+        else if (currentPlayer == 1)        //Player 2 turn
+        {
+            Debug.Log("Player 2 turn");
+
+            PieceManager1.player1Active = false;
+            PieceManager2.player2Active = true;
+            PieceManager3.player3Active = false;
+            PieceManager4.player4Active = false;
+
+        }
+
+        else if (currentPlayer == 2)         //Player 3 turn
+        {
+            Debug.Log("Player 3 turn");
+
+            PieceManager1.player1Active = false;
+            PieceManager2.player2Active = false;
+            PieceManager3.player3Active = true;
+            PieceManager4.player4Active = false;
+
+        }
+
+        else if (currentPlayer == 3)           //Player 4 turn
+        {
+            Debug.Log("Player 4 turn");
+
+            PieceManager1.player1Active = false;
+            PieceManager2.player2Active = false;
+            PieceManager3.player3Active = false;
+            PieceManager4.player4Active = true;
+
+        }
+    }
 }
--- Assets/Scripts/Pieces/Player2Piece3.cs
+++ Assets/Scripts/Pieces/Player2Piece3.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player2Piece3 : MonoBehaviour
 {
@@ -27,9 +28,161 @@ public class Player2Piece3 : MonoBehaviour
                     GameManager.currentPlayer = 2;
                     GameManager.currentSquare = curSquare;
 
-                    //call checkmovement
+                    bool anyMovement = checkMovement(CardDeck.card, curSquare, PieceManager2.startSquare, GetComponent<Renderer>());
                 }
             }
         }
+    }
+
+    public bool checkMovement(int card, int curSquare, int startSquare, Renderer piece)
+    {
+        bool anyMovement = false;
+
+        #region move from start
+        if (card == 1 || card == 2)
+        {
+            GameObject startButton = GameObject.Find("Move From Start");
+            startButton.GetComponentInChildren<Text>().text = "Move From Start";
+            Sprite UISprite = Resources.Load("unity_builtin_extra") as Sprite;
+            startButton.GetComponent<Image>().sprite = UISprite;
+
+            if (!(piece.tag == "Start"))
+            {
+                startButton.GetComponent<Button>().interactable = false;
+            }
+            else
+                startButton.GetComponent<Button>().interactable = true;
+            anyMovement = true;
+        }
+        #endregion
+
+        #region move forward
+        if (card == 1 || card == 2 || card == 3 || card == 5 || card == 7 || card == 8 || card == 10 || card == 11 || card == 12)
+        {
+            GameObject forButton = GameObject.Find("Move Forward");
+            forButton.GetComponentInChildren<Text>().text = "Move Forward";
+            Sprite UISprite = Resources.Load("unity_builtin_extra") as Sprite;
+            forButton.GetComponent<Image>().sprite = UISprite;
+
+            if (piece.tag == "Normal" && curSquare > (startSquare + 46) && curSquare < (startSquare + 58))
+            {
+                if (curSquare + card > startSquare + 64) // won't fit in the home spaces
+                {
+                    forButton.GetComponent<Button>().interactable = false;
+                }
+                else
+                {
+                    forButton.GetComponent<Button>().interactable = true;
+                    anyMovement = true;
+                }
+            }
+            else if (piece.tag == "Safe")
+            {
+                if ((curSquare % 6) + card > 5)
+                    forButton.GetComponent<Button>().interactable = false;
+                else
+                {
+                    forButton.GetComponent<Button>().interactable = true;
+                    anyMovement = true;
+                }
+            }
+            else if (piece.tag == "Home" || piece.tag == "Start")
+            {
+                forButton.GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                forButton.GetComponent<Button>().interactable = true;
+                anyMovement = true;
+            }
+        }
+        #endregion
+
+        #region move backward
+        if (card == 4 || card == 10)
+        {
+            GameObject backButton = GameObject.Find("Move Backward");
+            backButton.GetComponentInChildren<Text>().text = "Move Backward";
+            Sprite UISprite = Resources.Load("unity_builtin_extra") as Sprite;
+            backButton.GetComponent<Image>().sprite = UISprite;
+
+            if (piece.tag == "Normal" || piece.tag == "Safe")
+            {
+                backButton.GetComponent<Button>().interactable = true;
+                anyMovement = true;
+            }
+            else
+                backButton.GetComponent<Button>().interactable = false;
+        }
+        #endregion
+
+        #region swap sorry
+        if (card == 13)
+        {
+            if (piece.tag != "Start")
+            {
+
+            }
+            //all other players are not on normal squares
+            //change this for each player
+            else if (GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+            {
+
+            }
+            else
+            {
+                anyMovement = true;
+            }
+        }
+        #endregion
+
+        #region swap 11
+        if (card == 11)
+        {
+            GameObject swapButton = GameObject.Find("Swap");
+            swapButton.GetComponentInChildren<Text>().text = "Swap";
+            Sprite UISprite = Resources.Load("unity_builtin_extra") as Sprite;
+            swapButton.GetComponent<Image>().sprite = UISprite;
+
+            if (piece.tag != "Normal")
+            {
+                swapButton.GetComponent<Button>().interactable = false;
+            }
+            //all other players are not on normal squares
+            //change this for each player
+            else if (GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                 GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+            {
+                swapButton.GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                anyMovement = true;
+                swapButton.GetComponent<Button>().interactable = true;
+            }
+        }
+        #endregion
+        return anyMovement;
     }
 }
