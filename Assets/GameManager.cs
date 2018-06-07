38e1749cd498eb8f9ffd3a898ee355c3e0fec427
ONBASE\jzucca <jzucca>
2018-06-07 11:36:43 -0400

    Added scripts to all pieces, check and notify forfeit, player alabel

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,20 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
-    // Use this for initialization
-
-    [SerializeField]
-    private GameObject player1;
-    [SerializeField]
-    private GameObject player2;
-    [SerializeField]
-    private GameObject player3;
-    [SerializeField]
-    private GameObject player4;
+    public GameObject playerLabel;
     public static GameObject currentPiece;
     public static int currentPlayer = 0;
     public static int currentSquare;
@@ -39,43 +31,324 @@ public class GameManager : MonoBehaviour
         if (currentPlayer == 0)             //Player 1 turn
         {
             Debug.Log("Player 1 turn");
-            PieceManager1.player1Active = true;
-            PieceManager2.player2Active = false;
-            PieceManager3.player3Active = false;
-            PieceManager4.player4Active = false;
+
+            bool piece1 = checkMovement(CardDeck.card, Player1Piece1.curSquare, PieceManager1.startSquare, GameObject.Find("p1_Game_Piece_1"), 1);
+            bool piece2 = checkMovement(CardDeck.card, Player1Piece2.curSquare, PieceManager1.startSquare, GameObject.Find("p1_Game_Piece_2"), 1);
+            bool piece3 = checkMovement(CardDeck.card, Player1Piece3.curSquare, PieceManager1.startSquare, GameObject.Find("p1_Game_Piece_3"), 1);
+            bool piece4 = checkMovement(CardDeck.card, Player1Piece4.curSquare, PieceManager1.startSquare, GameObject.Find("p1_Game_Piece_4"), 1);
+
+            if (!piece1 && !piece2 && !piece3 && !piece4)
+            {
+                EditorUtility.DisplayDialog("Cannot Move", "You are not able to move any of your pieces. You must forfeit your turn.", "OK");
+                playerLabel = GameObject.Find("PlayerLabel");
+                playerLabel.GetComponent<Text>().text = "Player 2";
+                CardDeck.cardCount = 0;
+            }
+            else
+            {
+                PieceManager1.player1Active = true;
+                PieceManager2.player2Active = false;
+                PieceManager3.player3Active = false;
+                PieceManager4.player4Active = false;
+            }
         }
 
         else if (currentPlayer == 1)        //Player 2 turn
         {
             Debug.Log("Player 2 turn");
 
-            PieceManager1.player1Active = false;
-            PieceManager2.player2Active = true;
-            PieceManager3.player3Active = false;
-            PieceManager4.player4Active = false;
+            bool piece1 = checkMovement(CardDeck.card, Player2Piece1.curSquare, PieceManager2.startSquare, GameObject.Find("p2_Game_Piece_1"), 2);
+            bool piece2 = checkMovement(CardDeck.card, Player2Piece2.curSquare, PieceManager2.startSquare, GameObject.Find("p2_Game_Piece_2"), 2);
+            bool piece3 = checkMovement(CardDeck.card, Player2Piece3.curSquare, PieceManager2.startSquare, GameObject.Find("p2_Game_Piece_3"), 2);
+            bool piece4 = checkMovement(CardDeck.card, Player2Piece4.curSquare, PieceManager2.startSquare, GameObject.Find("p2_Game_Piece_4"), 2);
 
+            if (!piece1 && !piece2 && !piece3 && !piece4)
+            {
+                EditorUtility.DisplayDialog("Cannot Move", "You are not able to move any of your pieces. You must forfeit your turn.", "OK");
+                playerLabel = GameObject.Find("PlayerLabel");
+                playerLabel.GetComponent<Text>().text = "Player 3";
+                CardDeck.cardCount = 0;
+            }
+            else
+            {
+                PieceManager1.player1Active = false;
+                PieceManager2.player2Active = true;
+                PieceManager3.player3Active = false;
+                PieceManager4.player4Active = false;
+            }
         }
 
         else if (currentPlayer == 2)         //Player 3 turn
         {
             Debug.Log("Player 3 turn");
 
-            PieceManager1.player1Active = false;
-            PieceManager2.player2Active = false;
-            PieceManager3.player3Active = true;
-            PieceManager4.player4Active = false;
+            bool piece1 = checkMovement(CardDeck.card, Player3Piece1.curSquare, PieceManager3.startSquare, GameObject.Find("p3_Game_Piece_1"), 3);
+            bool piece2 = checkMovement(CardDeck.card, Player3Piece2.curSquare, PieceManager3.startSquare, GameObject.Find("p3_Game_Piece_2"), 3);
+            bool piece3 = checkMovement(CardDeck.card, Player3Piece3.curSquare, PieceManager3.startSquare, GameObject.Find("p3_Game_Piece_3"), 3);
+            bool piece4 = checkMovement(CardDeck.card, Player3Piece4.curSquare, PieceManager3.startSquare, GameObject.Find("p3_Game_Piece_4"), 3);
+
+            if (!piece1 && !piece2 && !piece3 && !piece4)
+            {
+                EditorUtility.DisplayDialog("Cannot Move", "You are not able to move any of your pieces. You must forfeit your turn.", "OK");
+                playerLabel = GameObject.Find("PlayerLabel");
+                playerLabel.GetComponent<Text>().text = "Player 4";
+                CardDeck.cardCount = 0;
+            }
+            else
+            {
+                PieceManager1.player1Active = false;
+                PieceManager2.player2Active = false;
+                PieceManager3.player3Active = true;
+                PieceManager4.player4Active = false;
+            }
 
         }
 
         else if (currentPlayer == 3)           //Player 4 turn
         {
             Debug.Log("Player 4 turn");
+            
 
-            PieceManager1.player1Active = false;
-            PieceManager2.player2Active = false;
-            PieceManager3.player3Active = false;
-            PieceManager4.player4Active = true;
+            bool piece1 = checkMovement(CardDeck.card, Player4Piece1.curSquare, PieceManager4.startSquare, GameObject.Find("p4_Game_Piece_1"), 4);
+            bool piece2 = checkMovement(CardDeck.card, Player4Piece2.curSquare, PieceManager4.startSquare, GameObject.Find("p4_Game_Piece_2"), 4);
+            bool piece3 = checkMovement(CardDeck.card, Player4Piece3.curSquare, PieceManager4.startSquare, GameObject.Find("p4_Game_Piece_3"), 4);
+            bool piece4 = checkMovement(CardDeck.card, Player4Piece4.curSquare, PieceManager4.startSquare, GameObject.Find("p4_Game_Piece_4"), 4);
 
+            if (!piece1 && !piece2 && !piece3 && !piece4)
+            {
+                EditorUtility.DisplayDialog("Cannot Move", "You are not able to move any of your pieces. You must forfeit your turn.", "OK");
+                playerLabel = GameObject.Find("PlayerLabel");
+                playerLabel.GetComponent<Text>().text = "Player 1";
+                CardDeck.cardCount = 0;
+            }
+            else
+            {
+                PieceManager1.player1Active = false;
+                PieceManager2.player2Active = false;
+                PieceManager3.player3Active = false;
+                PieceManager4.player4Active = true;
+            }
         }
+    }
+
+    public bool checkMovement(int card, int curSquare, int startSquare, GameObject piece, int player)
+    {
+        bool anyMovement = false;
+
+        #region move from start
+        if (card == 1 || card == 2)
+        {
+            if (piece.tag == "Start")
+                anyMovement = true;
+        }
+        #endregion
+
+        #region move forward
+        if (card == 1 || card == 2 || card == 3 || card == 5 || card == 7 || card == 8 || card == 10 || card == 11 || card == 12)
+        {
+            if (piece.tag == "Normal" && curSquare > (startSquare + 46) % 60 && curSquare < (startSquare + 58) % 60)
+            {
+                if (curSquare + card <= startSquare + 64) // won't fit in the home spaces
+                    anyMovement = true;
+            }
+            else if (piece.tag == "Safe")
+            {
+                if ((curSquare % 6) + card <= 5)
+                    anyMovement = true;
+            }
+            else if (piece.tag == "Normal" || piece.tag == "Safe")
+                anyMovement = true;
+        }
+        #endregion
+
+        #region move backward
+        if (card == 4 || card == 10)
+        {
+            if (piece.tag == "Normal" || piece.tag == "Safe")
+                anyMovement = true;
+        }
+        #endregion
+
+        #region swap sorry
+        if (card == 13)
+        {
+            if (piece.tag != "Start")
+            {
+
+            }
+            //all other players are not on normal squares
+            else if (player == 1)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 2)
+            {
+                if (GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 3)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 4)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+        }
+        #endregion
+
+        #region swap 11
+        if (card == 11)
+        {
+            if (piece.tag != "Normal")
+            {
+            }
+            //all other players are not on normal squares
+            else if (player == 1)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                         GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 2)
+            {
+                if (GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 3)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p4_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+            else if (player == 4)
+            {
+                if (GameObject.Find("p2_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p2_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p3_Game_Piece_4").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_1").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_2").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_3").tag != "Normal" &&
+                        GameObject.Find("p1_Game_Piece_4").tag != "Normal")
+                {
+
+                }
+                else
+                    anyMovement = true;
+            }
+        }
+        #endregion
+        return anyMovement;
     }
 }
